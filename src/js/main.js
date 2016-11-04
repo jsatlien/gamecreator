2ae936a9741b4a2c3ce9bb9aed361fc46de1875e
Jack Storrs <jackstorrs8@gmail.com>
2016-11-04 16:12:42 -0400

    added a 'game' class that will track and change the location, updated index.html.

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,9 @@
 import $ from 'jquery';
 import _ from 'lodash';
+
+import { Location } from '../js/model/location.js'
+
+var start = new Location ('The Weathered Tower', 0, [3,4,5], 1,'http://cdn.wallpapersafari.com/67/48/hcHPLl.jpg');
+
+start.updateDisplay()
+console.log(start)
--- src/js/model/location.js
+++ src/js/model/location.js
@@ -2,17 +2,33 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 
-class Location {
-  constructor (previous, adjacentArray, id) {
-  this.previous = previous;
-  this.adjacent = adjacentArray;
-  this.id = id;
+class Game {
+  constructor (currentLocation, reverseCount) {
+      this.currentLocation = currentLocation || 0;
+      this.reverseCount = reverseCount || 3;
+      // reverseCount is the number of times a player can choose to go back
+      // to the previous location.
   }
-  changeLocation(id) {
+  changeLocation(location) {
+      // $('#make-decision').click()
+  }
+}
 
+class Location {
+  constructor (name, previous, adjacentArray, id, imageUrl) {
+    this.name = name;
+    this.previous = previous;
+    this.adjacent = adjacentArray;
+    this.id = id;
+    this.imageUrl = imageUrl;
+  }
 
+  updateDisplay() {
+      $('#game-display').html(`<img src="${this.imageUrl}" alt="Image Not Found"/>`)
   }
 };
+
+// write a function that changes
 
 
 export { Location };
